b17b67dc86d60f1d1e90591a1bb5a953b6fc45ec
chandra8120 <chandrasekhar8120@gmail.com>
2026-01-24 15:21:32 +0530

    audio chats

--- src/Context.jsx
+++ src/Context.jsx
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 const CallContext = createContext();
 export const useCall = () => useContext(CallContext);
 
-const socket = io("http://localhost:5000");
+const socket = io("srv-d5q7pfh4tr6s73d99v9g");
 
 const config = {
   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
