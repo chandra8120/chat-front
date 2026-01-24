d5700a72faef8244b7ad12308748f6a545c922e7
chandra8120 <chandrasekhar8120@gmail.com>
2026-01-24 14:02:58 +0530

    audio chat implementss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chatting-wun1.onrender.com");
 
 const config = {
   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
